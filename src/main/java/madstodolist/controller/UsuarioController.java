af874764cf2b8bf1dea3cc845b7499c98d5205c7
Karina Arichavala <karina.arichavala@epn.edu.ec>
2025-06-10 16:48:08 -0500

    protección de vistas completo

--- src/main/java/madstodolist/controller/UsuarioController.java
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -7,31 +7,61 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import madstodolist.authentication.ManagerUserSession;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+
 import java.util.List;
 
 @Controller
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final ManagerUserSession managerUserSession;
 
-    public UsuarioController(UsuarioService usuarioService) {
+    public UsuarioController(UsuarioService usuarioService, ManagerUserSession managerUserSession) {
         this.usuarioService = usuarioService;
+        this.managerUserSession = managerUserSession;
     }
 
     @GetMapping("/registrados")
     public String verUsuariosRegistrados(Model model) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (idUsuarioLogeado == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No estás logeado");
+        }
+
+        UsuarioData usuario = usuarioService.findById(idUsuarioLogeado);
+        if (!Boolean.TRUE.equals(usuario.getEsAdmin())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Acceso no autorizado. Solo administradores.");
+        }
+
         List<UsuarioData> usuarios = usuarioService.findAllUsuarios();
         model.addAttribute("usuarios", usuarios);
-        return "registrados"; // Thymeleaf buscará registrados.html
+        return "registrados";
     }
+
     
     @GetMapping("/registrados/{id}")
     public String verDescripcionUsuario(@PathVariable Long id, Model model) {
-        UsuarioData usuario = usuarioService.findById(id);
-        if (usuario == null) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (idUsuarioLogeado == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No estás logeado");
+        }
+
+        UsuarioData usuario = usuarioService.findById(idUsuarioLogeado);
+        if (!Boolean.TRUE.equals(usuario.getEsAdmin())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Acceso no autorizado. Solo administradores.");
+        }
+
+        UsuarioData usuarioConsultado = usuarioService.findById(id);
+        if (usuarioConsultado == null) {
             return "redirect:/registrados?error=notfound";
         }
-        model.addAttribute("usuario", usuario);
+
+        model.addAttribute("usuario", usuarioConsultado);
         return "descripcionUsuario";
     }
+
 }
